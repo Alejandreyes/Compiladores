9167f2cc64597b7fdd3117f9d55b7506d10c0658
Alejandro Anzures <alejandreyes36@gmail.com>
2018-03-07 23:35:12 -0600

    Fizzbuzz python

---
+++ proyectos/proyecto01/src/ficheros/fz_error_identacion.py
@@ -0,0 +1,15 @@
+def fizzbuzz(limite):
+    i = 1
+    while i <= limite:
+        valor = True
+        if (i%3 == 0 and i%5 == 0):
+            print('fizzbuzz')
+            valor = False
+        if i % 3 == 0 :
+            print('fizz')
+            valor = False
+        if print('buzz')
+            valor = False
+        if(valor):
+            print(i)
+        i+=1
